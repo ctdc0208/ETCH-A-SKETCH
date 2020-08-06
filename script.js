f271fa0de928d3b4bb78691d7c4c4d21279a4987
nexus0208 <christiandelacruz0208@gmail.com>
2020-08-06 10:02:57 +0800

    .

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-//variable for grid container and brush option
 let grid = document.getElementById('grid');
 let brushColor = 'default';
 
-//buttons
+
 const reset = document.getElementById('reset');
 const resize = document.getElementById('resize');
 const colors = document.getElementById('colors');
 const shades = document.getElementById('fadeIn');
 const standard = document.getElementById('black');
 
-//event listeners
+
 reset.addEventListener('click', clearSquares);
 resize.addEventListener('click', resizeGrid);
 colors.addEventListener('click', brushSet => brushColor = 'random');
 shades.addEventListener('click', brushSet => brushColor = 'greyScale');
 standard.addEventListener('click', brushSet => brushColor = 'default');
 
-//functions
-//creates grid based on size dimension parameter
+
 function createGrid(rowCol) {
     grid.style.setProperty('--grid-rows', rowCol);
     grid.style.setProperty('--grid-cols', rowCol);
@@ -30,7 +28,6 @@ function createGrid(rowCol) {
     }
 }
 
-//used to reset the grid when pressing the reset button
 function clearSquares() {
     document.querySelectorAll('.square').forEach(square => {
         square.classList.remove('default');
@@ -40,27 +37,25 @@ function clearSquares() {
     })
 }
 
-//used to resize the grid when pressing the resize button
 function resizeGrid() {
     let size = Number(prompt("Please enter the desired grid size:\n(e.g., 16 will result in a 16x16 grid)", '16'));
 
-    //if size is an invalid input, default to 16x16 grid
+
     if(isNaN(size)===true || size <= 0) {
         size=16;
     }
 
-    //deletes existing grid div, then replaces it with new one and appropriate id for styling
+
     grid.remove();
     grid = document.createElement('div');
     grid.id='grid';
 
-    //adds newly created grid div to parent container, then creates the grid according to the user specified size
     let container = document.getElementById('container');
     container.appendChild(grid);
     createGrid(size);
 }
 
-//used to adjust the color scheme used when interacting with the grid
+
 function colorChange(e) {
     if(brushColor === 'default') {
         e.target.classList.add('default');
@@ -75,7 +70,6 @@ function colorChange(e) {
     }
 }
 
-//used for the random color scheme
 function randomColors(e) {
     let color = '#'+Math.floor(Math.random()*16777215).toString(16);
         e.target.style.setProperty('--rand-hex', color);
@@ -83,7 +77,7 @@ function randomColors(e) {
         e.target.classList.remove('greyScale');
 }
 
-//used for the greyscale color scheme
+
 function fadeIn(e) {
     e.target.classList.add('greyScale');
     e.target.classList.remove('random')
@@ -94,5 +88,5 @@ function fadeIn(e) {
     }
 }
 
-//create default grid
-createGrid(16); 
+
+createGrid(16);
